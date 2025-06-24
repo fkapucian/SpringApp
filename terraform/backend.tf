008d28c2ba03a749d5dadc2d29ac0d11123036d7
fkapucian <f.kapucian@gmail.com>
2025-06-24 13:31:26 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-spring-boot-app"
+    bucket         = "terraform-state-spring-boot-app-fkapucian"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
